f55d32b1d04d863e14ea31705969da836fe22b1a
Ben Cassell <bcassell22@jcu.edu>
2021-04-09 19:33:49 -0400

    Implement new ProjectManager design
    Removed obsolete methods "createName" and "checkName"
    Added new openProjectFile method
    Updated the saveProject method
    Added documentation to all methods in the class

--- marshmelloSongGenerator/src/marshmelloSongGenerator2/ProjectManager.java
+++ marshmelloSongGenerator/src/marshmelloSongGenerator2/ProjectManager.java
@@ -1,17 +1,29 @@
 package marshmelloSongGenerator2;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class ProjectManager extends StorageManager {
 	
-	private void saveProject() {
+	/**
+	 * Converts a given ArrayList<File> of soundFiles used to create a finished song project into a savable text file.
+	 * Will create a text file named after the given projectName and save the contents of the ArrayList in the file.
+	 * @param projectData - The given ArrayList of sound files used in a finished song project
+	 * @param projectName - The name of the project to be saved
+ 	 */
+	public void saveProject(ArrayList<File> projectData, String projectName) {
 		
 	}
 	
-	private void createName() {
+	/**
+	 * Opens the given project file and reads it contents.
+	 * Uses the list in the project file to create an arrayList of all the sound files listed in the project file, in the order they were listed.
+	 * @param project
+	 * @return Returns an ArrayList of type File
+	 */
+	public ArrayList<File> openProjectFile(File project) {
 		
-	}
-	
-	private void checkName() {
-		
+		return null;
 	}
 
 }
