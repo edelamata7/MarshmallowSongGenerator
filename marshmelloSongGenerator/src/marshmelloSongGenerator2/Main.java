4cdac4cb38715607402785b3e91f2380919e28be
Ben Cassell <bcassell22@jcu.edu>
2021-04-09 19:21:42 -0400

    Implement new Main design

--- marshmelloSongGenerator/src/marshmelloSongGenerator2/Main.java
+++ marshmelloSongGenerator/src/marshmelloSongGenerator2/Main.java
@@ -2,18 +2,13 @@ package marshmelloSongGenerator2;
 
 public class Main {
 
+	/**
+	 * Program's driver method
+	 * Should start the interface
+	 * @param args
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
-		//comment
-		
-		//Enrique
-
-		//Ben
-    
-		//gavin
-		
-		//John
 	}
 
 }
