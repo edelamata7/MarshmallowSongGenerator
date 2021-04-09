a19057c8c438aa200a0b369c60a542e6b3c45169
Ben Cassell <bcassell22@jcu.edu>
2021-04-09 19:56:28 -0400

    Implement new CreateSong design
    Makes class abstract
    Delete the obsolete "getFiles", "selectAndAddFile", "checkLength,"
    startProject," and "saveProject" methods
    Added new "createCompleteWavFile", "createProjectFile",
    "compileSoundFiles", and "createProjectList" methods
    Added documentation to the class

--- marshmelloSongGenerator/src/marshmelloSongGenerator2/CreateSong.java
+++ marshmelloSongGenerator/src/marshmelloSongGenerator2/CreateSong.java
@@ -1,36 +1,62 @@
 package marshmelloSongGenerator2;
 
+import java.io.File;
+import java.util.ArrayList;
 
-public class CreateSong {
+import wavFile.WavFile;
+
+public abstract class CreateSong {
 	
+	/**
+	 * Allows this class to access files in storage and save/open project files
+	 */
 	private ProjectManager projectManager;
 	
 	public CreateSong() {
 		projectManager = new ProjectManager();
 	}
 	
-	public void createProject() {
+	/**
+	 * Calls the methods in the createSong class to create a sound file and a project file using the information in the given projectList
+	 * @param projectList - contains the list of files/wavFiles that are needed to create the sound and project files
+	 * @param filename - what to save the new project as
+	 */
+	public void createProject(ProjectList projectList, String filename) {
 		
 	}
 	
-	private void getFiles() {
+	/**
+	 * Combines all the wavFiles in the given list into one single wavFile
+	 * @param wavFileList - a list of the wavFiles to add to the new project
+	 * @param filename - what to save the new wavFile as
+	 */
+	private void createCompleteWavFile(ArrayList<WavFile> wavFileList, String filename) {
 		
 	}
 	
-	private void selectAndAddFile() {
+	/**
+	 * Pass the given values into the projectManager to create/save a project file
+	 * @param fileList
+	 * @param filename
+	 */
+	private void createProjectFile(ArrayList<File> fileList, String filename) {
 		
 	}
 	
-	private boolean checkLength() {
-		return true;
+	/**
+	 * Takes an arrayList of File objects to convert into an arrayList of wavFile objects
+	 * @param soundFiles - The list of file objects to turn into an array of wavFile objects
+	 * @return Returns an arrayList of wavFile objects created using the file objects in the given soundFiles array
+	 */
+	public ArrayList<WavFile> compileSoundFiles(ArrayList<File> soundFiles) { //Should throw an exception if the given file type cannot be converted in to a wavFile? need to look into that further
+		return null;
 	}
 	
-	private void startProject() {
-		
-	}
-	
-	private void saveProject() {
-		
-	}
+	/**
+	 * Takes a given arrayList of file objects and converts it into a projectList
+	 * @param soundFiles
+	 * @return
+	 */
+	protected abstract ProjectList createProjectList(ArrayList<File> soundFiles);
 
 }
