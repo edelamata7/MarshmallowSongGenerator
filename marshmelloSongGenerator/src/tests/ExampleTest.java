2c88a84b026f1dbfb54cbdffaf0b9794c58fe3a8
Ben Cassell <bcassell22@jcu.edu>
2021-04-15 16:46:06 -0400

    Create the tests package and testFiles folder
    Added several test files to the testFiles folder and an exampleTest
    class to the tests package

---
+++ marshmelloSongGenerator/src/tests/ExampleTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import marshmelloSongGenerator2.*;
+
+public class ExampleTest {
+
+	/**
+	 * Just an example of how to write a system test using this tests package setup and the testFiles folder
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Get a file from the testFiles folder to test
+		File testFile = new File("./testFiles/testFile1_TurningObjectsIntoPercussion.wav");
+		
+		assert(testFile.exists()) : "Error: Cannot find file"; //Test if the file exists
+	}
+
+}
