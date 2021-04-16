8660b716d2c5c6e7a3f63e6597a971c11fe779d6
Ben Cassell <bcassell22@jcu.edu>
2021-04-16 17:30:33 -0400

    Implement createCompleteWavFile method & test

--- marshmelloSongGenerator/src/tests/CreateSongTests.java
+++ marshmelloSongGenerator/src/tests/CreateSongTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import marshmelloSongGenerator2.*;
@@ -10,8 +11,31 @@ public class CreateSongTests {
 
 	public static void main(String[] args) {
 		CreateSongTests testDriver = new CreateSongTests();
-		testDriver.testCompileSoundFiles();
+		//testDriver.testCompileSoundFiles();
+		testDriver.testCreateCompleteWavFile();
+	}
+	
+	public void testCreateCompleteWavFile() {
+		ArrayList<File> soundFiles = new ArrayList<File>();
+		ArrayList<WavFile> wavFiles = new ArrayList<WavFile>();
 		
+		try {
+			wavFiles.add(WavFile.openWavFile(new File("./testFiles/testFile1_TurningObjectsIntoPercussion.wav")));
+			wavFiles.add(WavFile.openWavFile(new File("./testFiles/testFile2_BassLoopsWithDrums.wav")));
+			wavFiles.add(WavFile.openWavFile(new File("./testFiles/testFile3_BassLoopsNoDrums.wav")));
+			wavFiles.add(WavFile.openWavFile(new File("./testFiles/testFile4_DiscoFunkLoopsNoDrums.wav")));
+			wavFiles.add(WavFile.openWavFile(new File("./testFiles/testFile5_DiscoFunkLoopsWithDrums.wav")));
+			
+		} catch (IOException | WavFileException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		ProjectList test1 = new ProjectList(wavFiles, soundFiles);
+		
+		CreateSong createSongTest = new SongGenerator();
+		createSongTest.createProject(test1, "RESULT_completeWavFileTest1");
+		System.out.println("TEST COMPLETE");
 	}
 	
 	public void testCompileSoundFiles() {
