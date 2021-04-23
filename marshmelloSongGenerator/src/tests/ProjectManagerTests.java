23c8d01418a0913166fc115ff5e8e31420a3f7de
Ben Cassell <bcassell22@jcu.edu>
2021-04-22 20:16:33 -0400

    Create ProjectManagerTests & edit StorageManager
    Initialized storageLocation in StorageManager to "." to solve a null
    error in StorageManager's constructor

---
+++ marshmelloSongGenerator/src/tests/ProjectManagerTests.java
@@ -0,0 +1,19 @@
+package tests;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import marshmelloSongGenerator2.*;
+
+public class ProjectManagerTests {
+	
+	
+	public static void main(String[] args) {
+		
+		ProjectManager projectManager = new ProjectManager();
+		
+		System.out.println("Test Complete");
+		
+	}
+
+}
