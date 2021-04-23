547d3c6adc4b09a6ffaaddcbe6e2a83991de9751
Ben Cassell <bcassell22@jcu.edu>
2021-04-22 21:44:36 -0400

    Implement saveProject method

--- marshmelloSongGenerator/src/marshmelloSongGenerator2/ProjectManager.java
+++ marshmelloSongGenerator/src/marshmelloSongGenerator2/ProjectManager.java
@@ -1,7 +1,10 @@
 package marshmelloSongGenerator2;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -10,35 +13,55 @@ import java.util.Scanner;
 public class ProjectManager extends StorageManager {
 	
 	/**
+	 * Temporary solution to setting the storageLocation string while changing as little of storageManager as possible
+	 */
+	public ProjectManager() {
+		storageLocation = ".\\testFiles";
+	}
+	
+	/**
 	 * Converts a given ArrayList<File> of soundFiles used to create a finished song project into a saveable text file.
 	 * Will create a text file named after the given projectName and save the contents of the ArrayList in the file.
 	 * @param projectData - The given ArrayList of sound files used in a finished song project
 	 * @param projectName - The name of the project to be saved
  	 */
 	public void saveProject(ArrayList<File> projectData, String projectName) {
-		
-		/*
-		 * Tests:
-		 * test that it creates a new, blank text file
-		 */
-		
-		File project = new File("");
-		
-		assert (new File(storageLocation+"\\projectFiles\\"+projectName+".txt").exists()) : "Error: No file with the given filename exists in the projectFiles folder"; //Checks that a file can be found in the project files folder that matches the name of the newly created file
-		
-		
-		
-		
+		//Create the file object that's used to create the new text file
+		File project = new File(storageLocation+"\\projectFiles\\"+projectName+".txt");
 		
 		try {
-			Scanner myReader = new Scanner(project);
-			String data = myReader.nextLine();
-			assert (data == "MARSHMELLOW SONG GENERATOR PROJECT") : "Error: Project file indicator wasnt found"; //Checks that the indicator is included in the file
-			for(int i=0; i < projectData.size() && myReader.hasNextLine(); i++) {
-				data = myReader.nextLine();
-				assert(data == projectData.get(i).getName()) : "Error: projectFile contents don't match the given arrayList"; //Checks that each line of the project text file matches the files in the projectData arrayList
+			//Creates the text file
+			project.createNewFile();
+			FileWriter fileWriter = new FileWriter(project, false);
+			BufferedWriter writer = new BufferedWriter(fileWriter);
+			writer.write("MARSHMELLOW SONG GENERATOR PROJECT");
+			writer.newLine();
+			
+			for (File file : projectData) {
+				writer.write(file.getName());
+				writer.newLine();
 			}
-			myReader.close();
+			
+			writer.close();
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		//Checks that a file can be found in the project files folder that matches the name of the newly created file
+		assert (new File(storageLocation+"\\projectFiles\\"+projectName+".txt").exists()) : "Error: No file with the given filename exists in the projectFiles folder";
+		
+		try {
+			Scanner reader = new Scanner(project);
+			String data;
+			if (reader.hasNextLine()) {
+				data = reader.nextLine();
+				assert (data.matches("MARSHMELLOW SONG GENERATOR PROJECT")) : "Error: Project file indicator wasnt found"; //Checks that the indicator is included in the file
+				for(int i=0; i < projectData.size() && reader.hasNextLine(); i++) {
+					data = reader.nextLine();
+					assert(data.matches(projectData.get(i).getName())) : "Error: projectFile contents don't match the given arrayList"; //Checks that each line of the project text file matches the files in the projectData arrayList
+				}
+			}
+			reader.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
